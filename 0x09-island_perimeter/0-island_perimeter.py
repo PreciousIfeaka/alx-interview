fc8a00c75c013bb8208468c39d40106bcfe74999
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-12-22 13:52:29 +0000

    first task here

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""This module computes the perimeter ojf an island given in grid form
+"""
+
+
+def island_perimeter(grid):
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                # Check left
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+
+                # Check right
+                if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+                # Check up
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+
+                # Check down
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+
+    return perimeter
