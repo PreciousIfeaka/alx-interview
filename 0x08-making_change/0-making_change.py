c4e84c1e3aa2ae61be0b6b747123cf7f8dbc485b
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-12-22 13:26:45 +0000

    updated the algorithm

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,23 +6,24 @@
 
 
 def makeChange(coins, total):
-    # Base cases
     if total <= 0:
         return 0
-    if not coins:
-        return -1
 
-    # Initialize the result to a large value
-    result = float('inf')
-
-    # Try using each coin and recursively calculate the number of coins needed
-    for coin in coins:
-        if coin <= total:
-            subproblem_result = makeChange(coins, total - coin)
-            if subproblem_result != -1:
-                result = min(result, subproblem_result + 1)
-
-    '''If result is still initialized to a large value, no
-       combination of coins can make the total
+    '''Initialize an array to store the minimum number of coin for each
+       amount and also set the default value to be greater than any
+       possible number of coins
     '''
-    return result if result != float('inf') else -1
+    dp = [float('inf')] * (total + 1)
+
+    # Base case: 0 coins needed to make change for 0
+    dp[0] = 0
+
+    # Iterate over each coin and update the minimum number of coins needed
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    '''If dp[total] is still the initial value,
+       no combination of coins can make the total
+    '''
+    return dp[total] if dp[total] != float('inf') else -1
